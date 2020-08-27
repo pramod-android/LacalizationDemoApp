aa1af6e90f4b891a9cfc212c88e17962b2e8f7a4
pramod-aerobit <pramod@aerobithealth.com>
2020-08-27 13:16:36 +0530

    change language from applcation

--- app/src/main/java/com/marathideveloper/lacalizationdemoapp/MainActivity.kt
+++ app/src/main/java/com/marathideveloper/lacalizationdemoapp/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.marathideveloper.lacalizationdemoapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
+import android.os.LocaleList
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import java.lang.String.format
+import java.util.*
+
+// extend class Activity_Base() to change language programmatically
+//Activity_Base()
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        var result = LocaleList.getDefault()
+
+        Log.i("log","onCreate")
+
+
     }
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(ApplicationLanguageHelper.wrap(newBase!!, "mr"))
+        Log.i("log","attachBaseContext")
+    }
+
+
 }
